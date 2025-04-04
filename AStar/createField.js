d2d26471b707ca8a847404e661dad8f68b6a469b
salokrass@yandex.ru <salokrass@yandex.ru>
2025-04-04 18:18:03 +0700

    dynamic field in astar

--- AStar/createField.js
+++ AStar/createField.js
@@ -1,6 +1,12 @@
 
 let isStartPlaced = false;
 let isEndPlaced = false;
+const field = document.getElementById("field");
+
+let sizeField = 0;
+const urlFindRoute = 'http://localhost:5077/AStar/route';
+const urlGetMaze = 'http://localhost:5077/AStar/maze?size=';
+
 function selectStartOrEnd(event) {
     event.preventDefault();
     if(event.currentTarget.className === 'cell-start')
@@ -41,28 +47,85 @@ function selectUnavailableCells(event) {
     }
 }
 
-const field = document.getElementById("field");
-
-let sizeField = 0;
-function sizeFieldChange()
+async function sizeFieldChange()
 {
     sizeField = document.getElementById('sizeInput').value;
     field.innerHTML = '';
+
+    if(sizeField <=20)
+    {
+        field.style.width = 500 + 'px';
+        field.style.height = 500 + 'px';
+        field.style.columnGap = 10 + 'px';
+        field.style.rowGap = 10 + 'px';
+    }
+    else if(sizeField >20 && sizeField <= 35)
+    {
+        field.style.width = 700 + 'px';
+        field.style.height = 700 + 'px';
+        field.style.columnGap = 7 + 'px';
+        field.style.rowGap = 7 + 'px';
+    }
+    else if(sizeField >35 && sizeField <= 55)
+    {
+        field.style.width = 800 + 'px';
+        field.style.height = 800 + 'px';
+        field.style.columnGap = 1 + 'px';
+        field.style.rowGap = 1 + 'px';
+    }
+    else if(sizeField >55 && sizeField <= 75)
+    {
+        field.style.width = 900 + 'px';
+        field.style.height = 900 + 'px';
+        field.style.columnGap = 1 + 'px';
+        field.style.rowGap = 1 + 'px';
+    }
+    else if(sizeField >75 && sizeField <= 100)
+    {
+        field.style.width = 1000 + 'px';
+        field.style.height = 1000 + 'px';
+        field.style.columnGap = 1 + 'px';
+        field.style.rowGap = 1 + 'px';
+    }
+    else if(sizeField >100 && sizeField <= 150)
+    {
+        field.style.width = 1200 + 'px';
+        field.style.height = 1200 + 'px';
+        field.style.columnGap = 1 + 'px';
+        field.style.rowGap = 1 + 'px';
+    }
+    else if(sizeField >150 && sizeField <= 300)
+    {
+        field.style.width = 1500 + 'px';
+        field.style.height = 1500 + 'px';
+        field.style.columnGap = 0 + 'px';
+        field.style.rowGap = 0 + 'px';
+    }
     field.style.setProperty("grid-template-columns", 'repeat(' + sizeField + ', 1fr)');
     field.style.setProperty("grid-template-rows", 'repeat(' + sizeField + ', 1fr)');
 
-    for (let i = 0; i < sizeField*sizeField; i++)
+    let response = await fetch(urlGetMaze+sizeField);
+    let maze = await response.json();
+
+    for (let i = 0; i < sizeField; i++)
     {
-        let divElement = document.createElement("div");
-        divElement.addEventListener('contextmenu',  selectStartOrEnd, false);
-        divElement.addEventListener('click',  selectUnavailableCells);
-        divElement.className = 'cell-available';
-        field.appendChild(divElement);
+        for (let j = 0; j < sizeField; j++)
+        {
+            let divElement = document.createElement("div");
+            divElement.addEventListener('contextmenu',  selectStartOrEnd, false);
+            divElement.addEventListener('click',  selectUnavailableCells);
+            if(maze[i][j] === true)
+                divElement.className = 'cell-available';
+            else
+                divElement.className = 'cell-unavailable';
+
+            field.appendChild(divElement);
+        }
+
     }
 }
  async function calculateRoute() {
      const cells = field.children;
-     const url = 'http://localhost:5077/AStar';
      let matrix = [];
      let newList = [];
      for (let i = 0; i < cells.length; i++) {
@@ -86,7 +149,7 @@ function sizeFieldChange()
      }
      matrix.push(newList);
 
-     let response = await fetch(url, {
+     let response = await fetch(urlFindRoute, {
          method: 'POST',
          headers: {'Content-Type': 'application/json'},
          body: JSON.stringify(matrix)
@@ -97,10 +160,23 @@ function sizeFieldChange()
 
 function drawRoute(data)
 {
-    const cells = field.children;
-    let indexes = data['indexes'];
-    for (let i = 0; i < indexes.length-1; i++)
+    if(data !== null)
     {
-        cells[indexes[i].x + indexes[i].y * sizeField].className = "cell-rout";
+        const cells = field.children;
+        let indexes = data['indexes'];
+
+        for (let i = 0; i < indexes.length - 1; i++) {
+            cells[indexes[i].x + indexes[i].y * sizeField].className = "cell-rout";
+        }
     }
+}
+
+function clearField()
+{
+    const cells = field.children;
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].className = 'cell-available';
+    }
+    isStartPlaced = false;
+    isEndPlaced = false;
 }
